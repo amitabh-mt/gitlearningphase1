a73c12d404aec88c30597abcad2fddfeb084fecc
amitabh-mt <amitabhm.tripathi@gmail.com>
2026-07-04 13:25:34 +0530

    Update feature block in provider.tf
    feature block

--- provider.tf
+++ provider.tf
@@ -6,3 +6,7 @@ terraform {
     }
   }
 }
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
